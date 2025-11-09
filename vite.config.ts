e24892117dc0c5ab6bd27eb5bb98b9fc50e2cdc5
TippiBui <thaobui815@gmail.com>
2025-11-09 23:55:11 +0700

    build: update base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: 'https://thaophgg.github.io/fe-entry-test.git'
+  base: 'https://thaophgg.github.io/fe-entry-test'
 })
